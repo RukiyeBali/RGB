53cf7310aea9e2b48d2d3e737e5e4154e434b272
RukiyeBali <145977861+RukiyeBali@users.noreply.github.com>
2023-10-03 14:23:05 +0300

    Add files via upload

---
+++ Renk-değiştirme/Renk-değiştirme/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace Renk_değiştirme
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbRed = new System.Windows.Forms.TrackBar();
+            this.tbGreen = new System.Windows.Forms.TrackBar();
+            this.tbBlue = new System.Windows.Forms.TrackBar();
+            this.lblRed = new System.Windows.Forms.Label();
+            this.lblGreen = new System.Windows.Forms.Label();
+            this.lblBlue = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.tbRed)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbGreen)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbBlue)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbRed
+            // 
+            this.tbRed.Location = new System.Drawing.Point(12, 30);
+            this.tbRed.Maximum = 255;
+            this.tbRed.Name = "tbRed";
+            this.tbRed.Size = new System.Drawing.Size(593, 45);
+            this.tbRed.TabIndex = 0;
+            this.tbRed.Scroll += new System.EventHandler(this.tbRed_Scroll);
+            // 
+            // tbGreen
+            // 
+            this.tbGreen.Location = new System.Drawing.Point(12, 81);
+            this.tbGreen.Maximum = 255;
+            this.tbGreen.Name = "tbGreen";
+            this.tbGreen.Size = new System.Drawing.Size(593, 45);
+            this.tbGreen.TabIndex = 1;
+            this.tbGreen.Scroll += new System.EventHandler(this.tbGreen_Scroll);
+            // 
+            // tbBlue
+            // 
+            this.tbBlue.Location = new System.Drawing.Point(12, 132);
+            this.tbBlue.Maximum = 255;
+            this.tbBlue.Name = "tbBlue";
+            this.tbBlue.Size = new System.Drawing.Size(593, 45);
+            this.tbBlue.TabIndex = 2;
+            this.tbBlue.Scroll += new System.EventHandler(this.tbBlue_Scroll);
+            // 
+            // lblRed
+            // 
+            this.lblRed.AutoSize = true;
+            this.lblRed.Location = new System.Drawing.Point(611, 46);
+            this.lblRed.Name = "lblRed";
+            this.lblRed.Size = new System.Drawing.Size(35, 13);
+            this.lblRed.TabIndex = 3;
+            this.lblRed.Text = "label1";
+            // 
+            // lblGreen
+            // 
+            this.lblGreen.AutoSize = true;
+            this.lblGreen.Location = new System.Drawing.Point(611, 93);
+            this.lblGreen.Name = "lblGreen";
+            this.lblGreen.Size = new System.Drawing.Size(35, 13);
+            this.lblGreen.TabIndex = 4;
+            this.lblGreen.Text = "label2";
+            // 
+            // lblBlue
+            // 
+            this.lblBlue.AutoSize = true;
+            this.lblBlue.Location = new System.Drawing.Point(611, 147);
+            this.lblBlue.Name = "lblBlue";
+            this.lblBlue.Size = new System.Drawing.Size(35, 13);
+            this.lblBlue.TabIndex = 5;
+            this.lblBlue.Text = "label3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(696, 339);
+            this.Controls.Add(this.lblBlue);
+            this.Controls.Add(this.lblGreen);
+            this.Controls.Add(this.lblRed);
+            this.Controls.Add(this.tbBlue);
+            this.Controls.Add(this.tbGreen);
+            this.Controls.Add(this.tbRed);
+            this.Name = "Form1";
+            this.Text = "Ekranın Rengi Değişebilir...";
+            ((System.ComponentModel.ISupportInitialize)(this.tbRed)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbGreen)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbBlue)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar tbRed;
+        private System.Windows.Forms.TrackBar tbGreen;
+        private System.Windows.Forms.TrackBar tbBlue;
+        private System.Windows.Forms.Label lblRed;
+        private System.Windows.Forms.Label lblGreen;
+        private System.Windows.Forms.Label lblBlue;
+    }
+}
+
---
+++ Renk-değiştirme/Renk-değiştirme/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Renk_değiştirme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void tbRed_Scroll(object sender, EventArgs e)
+        {
+            int r, g, b;
+
+            r = tbRed.Value;
+            g = tbGreen.Value;
+            b = tbBlue.Value;
+
+            lblRed.Text=r.ToString();
+
+            this.BackColor=System.Drawing.Color.FromArgb(r,g,b);
+        }
+
+        private void tbGreen_Scroll(object sender, EventArgs e)
+        {
+            int r, g, b;
+
+            r = tbRed.Value;
+            g = tbGreen.Value;
+            b = tbBlue.Value;
+
+            lblGreen.Text = g.ToString();
+
+            this.BackColor = System.Drawing.Color.FromArgb(r, g, b);
+
+        }
+
+        private void tbBlue_Scroll(object sender, EventArgs e)
+        {
+            int r, g, b;
+
+            r = tbRed.Value;
+            g = tbGreen.Value;
+            b = tbBlue.Value;
+
+            lblBlue.Text = b.ToString();
+
+            this.BackColor = System.Drawing.Color.FromArgb(r, g, b);
+        }
+    }
+}
